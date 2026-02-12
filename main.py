1e9ba82a37510ed68935088c75306a612515d346
Lalith Reddy <lalithgrad@gmail.com>
2026-02-12 21:46:37 +0530

    integrated fastapi

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException
+from core.smart_queue import SmartQueue, Decision
+import asyncio
+
+app = FastAPI()
+
+sq = SmartQueue(max_workers=2, max_queue_size=3)
+
+@app.get("/api")
+async def protected_api(priority: str = "LOW"):
+    request_id = str(id(asyncio.current_task()))
+    decision = sq.admit_request(request_id, priority)
+    if decision == Decision.REJECT:
+        raise HTTPException(status_code=429, detail="Request Rejected - System Overloaded")
+    try:
+        await asyncio.sleep(5)
+        return {
+            "message": "Request processed successfully",
+            "priority": priority
+        }
+    finally:
+        sq.on_worker_complete()
